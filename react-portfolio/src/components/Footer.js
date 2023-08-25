f099eaeea03f82a84e32ba11838bc548fa6c0559
Beatriz Sandoval <beasaddress@gmail.com>
2023-08-24 17:38:52 -0700

    added empty components

---
+++ react-portfolio/src/components/Footer.js
@@ -0,0 +1,3 @@
+//WHEN I view the footer
+//THEN I am presented with text or icon links to the developer’s 
+//GitHub and LinkedIn profiles, 
---
+++ react-portfolio/src/components/pages/Portfolio.js
@@ -0,0 +1,4 @@
+//WHEN I am presented with the Portfolio section
+//THEN I see titled images of six of the developer’s 
+//applications with links to both the deployed applications 
+//and the corresponding GitHub repositories
